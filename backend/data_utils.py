536f0ebbc9f802bcea970fc79641856e5bf45daa
Charles Henville <henvillecharles@gmail.com>
2024-01-23 14:49:04 -0500

    Added longest delta

--- backend/data_utils.py
+++ backend/data_utils.py
@@ -8,9 +8,17 @@ def obliterate_long_delta(df, maxdelta):
     modded_df = df
     time_list = df['time'].tolist()
     last_time = time_list[0]
+
+    longest_delta = 0
+
     for idx, time in enumerate(time_list):
-        if time-last_time >= maxdelta:
+
+        newdelta = time-last_time
+        if newdelta > longest_delta:
+            longest_delta = newdelta
+        if  newdelta>= maxdelta:
             modded_df=df.drop(index=range(idx))
         last_time = time
+    print("Longest Delta: {delta}".format(delta=longest_delta))
 
     return modded_df
